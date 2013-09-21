30827fcff3a8ee5ba435fc946a374ed1c20a9739
Stefan Holzmüller <stefanholzmueller@github>
2013-09-21 20:43:01 +0200

    UI complete, but only a dummy sequence implemented

--- sequences/app.js
+++ sequences/app.js
@@ -6,6 +6,19 @@ module.value('numberOfGivenElements', 5);
 // config end
 
 module.controller('SequencesController', [ '$scope', 'numberOfSequences', 'numberOfGivenElements', function($scope, numberOfSequences, numberOfGivenElements) {
-	$scope.sequences = _.range(numberOfSequences);
+	$scope.guesses = new Array(numberOfSequences);
+	$scope.puzzles = _.range(numberOfSequences);
 	$scope.given = _.range(numberOfGivenElements);
+	
+	var basic = function(x) {
+		return x;
+	};
+	
+	$scope.sequences = _.range(numberOfSequences).map(function (_) {
+		return basic;
+	});
+	
+	$scope.check = function(i) {
+		return $scope.guesses[i] == $scope.sequences[i](numberOfGivenElements);
+	};
 } ]);
