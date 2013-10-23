6ae61cfc405fd9268e036722852cadddda5b2d62
Stefan Holzmüller <stefanholzmueller@github>
2013-10-23 17:33:08 +0200

    first tests with TypeScript

---
+++ calculator/calculator.js
@@ -0,0 +1,9 @@
+/// <reference path="angular.d.ts" />
+/// <reference path="underscore.d.ts" />
+"use strict";
+var app = angular.module("calculator", []);
+
+app.controller("CalculatorController", function ($scope) {
+    $scope.keypad = _.range(10);
+});
+//# sourceMappingURL=calculator.js.map
