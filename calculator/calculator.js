58a3dd4ab57a92e6e8d238477f0bb23bce2155d7
Stefan Holzmüller <stefanholzmueller@github>
2013-11-03 13:56:27 +0100

    handle adding to formula

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,29 +6,77 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-var Input = (function () {
-    function Input(label) {
+var Special = (function () {
+    function Special(label) {
         this.label = label;
     }
-    return Input;
+    Special.prototype.display = function () {
+        return this.label;
+    };
+    return Special;
 })();
 
-var Op = (function (_super) {
-    __extends(Op, _super);
-    function Op() {
-        _super.apply(this, arguments);
-    }
-    return Op;
-})(Input);
-
-var Num = (function (_super) {
-    __extends(Num, _super);
+var Num = (function () {
     function Num(value) {
-        _super.call(this, value.toString());
         this.value = value;
     }
+    Num.prototype.display = function () {
+        return this.value.toString();
+    };
     return Num;
-})(Input);
+})();
+
+var BinaryOp = (function () {
+    function BinaryOp(label, precedence, fn) {
+        this.label = label;
+        this.precedence = precedence;
+        this.fn = fn;
+    }
+    BinaryOp.prototype.display = function () {
+        return this.label;
+    };
+    return BinaryOp;
+})();
+
+var AddOp = (function (_super) {
+    __extends(AddOp, _super);
+    function AddOp() {
+        _super.call(this, "+", 2, function (l, r) {
+            return l + r;
+        });
+    }
+    return AddOp;
+})(BinaryOp);
+
+var SubOp = (function (_super) {
+    __extends(SubOp, _super);
+    function SubOp() {
+        _super.call(this, "-", 2, function (l, r) {
+            return l - r;
+        });
+    }
+    return SubOp;
+})(BinaryOp);
+
+var MulOp = (function (_super) {
+    __extends(MulOp, _super);
+    function MulOp() {
+        _super.call(this, "*", 3, function (l, r) {
+            return l * r;
+        });
+    }
+    return MulOp;
+})(BinaryOp);
+
+var DivOp = (function (_super) {
+    __extends(DivOp, _super);
+    function DivOp() {
+        _super.call(this, "/", 3, function (l, r) {
+            return l / r;
+        });
+    }
+    return DivOp;
+})(BinaryOp);
 
 function evaluate(formula) {
     return 123;
@@ -40,26 +88,50 @@ app.controller("CalculatorController", function ($scope) {
     $scope.formula = [];
     $scope.result = "0";
     $scope.grid = [
-        [new Num(7), new Num(8), new Num(9), new Op("/")],
-        [new Num(4), new Num(5), new Num(6), new Op("*")],
-        [new Num(1), new Num(2), new Num(3), new Op("-")],
-        [new Num(0), new Input("."), new Input("="), new Op("+")]
+        [new Num(7), new Num(8), new Num(9), new DivOp()],
+        [new Num(4), new Num(5), new Num(6), new MulOp()],
+        [new Num(1), new Num(2), new Num(3), new SubOp()],
+        [new Num(0), new Special("."), new Special("="), new AddOp()]
     ];
 
+    function isNum(x) {
+        return x !== undefined && x.value !== undefined;
+    }
+
+    function addToFormula(k) {
+        var last = _.last($scope.formula);
+        if (isNum(k)) {
+            if (isNum(last)) {
+                var kValue = k.value;
+                $scope.formula.pop();
+                $scope.formula.push(new Num(last.value * 10 + kValue));
+            } else {
+                $scope.formula.push(k);
+            }
+        } else {
+            if (isNum(last)) {
+                $scope.formula.push(k);
+            } else {
+                $scope.formula.pop();
+                $scope.formula.push(k);
+            }
+        }
+    }
+
     $scope.press = function (key) {
-        if (key.label === "=") {
+        if (key.display() === "=") {
             $scope.result = evaluate($scope.formula);
-        } else if (key.label === ".") {
+        } else if (key.display() === ".") {
             // TODO decimal point
         } else {
-            $scope.formula.push(key);
+            addToFormula(key);
         }
     };
     $scope.$on("keypress", function (keyCode) {
         var pressedKey = String.fromCharCode(keyCode);
         _.each($scope.grid, function (row) {
             return _.each(row, function (key) {
-                if (key.label === pressedKey) {
+                if (key.display() === pressedKey) {
                     $scope.press(key);
                 }
             });
--- calculator/calculator.ts
+++ calculator/calculator.ts
@@ -1,18 +1,66 @@
 /// <reference path="lib/angular.d.ts" />
 /// <reference path="lib/underscore.d.ts" />
 
-class Input {
-	constructor(public label : string) {
+interface Input {
+	display()
+}
+
+class Special implements Input {
+	constructor(private label : string) {
+	}
+
+	display() {
+		return this.label;
 	}
 }
 
-class Op extends Input {
+class Num implements Input {
+	constructor(public value : number) {
+	}
 
+	display() {
+		return this.value.toString();
+	}
 }
 
-class Num extends Input {
-	constructor(public value : number) {
-		super(value.toString())
+class BinaryOp implements Input {
+	constructor(private label : string, public precedence : number, public fn) {
+	}
+
+	display() {
+		return this.label;
+	}
+}
+
+class AddOp extends BinaryOp {
+	constructor() {
+		super("+", 2, function (l, r) {
+			return l + r;
+		});
+	}
+}
+
+class SubOp extends BinaryOp {
+	constructor() {
+		super("-", 2, function (l, r) {
+			return l - r;
+		});
+	}
+}
+
+class MulOp extends BinaryOp {
+	constructor() {
+		super("*", 3, function (l, r) {
+			return l * r;
+		});
+	}
+}
+
+class DivOp extends BinaryOp { // TODO handle div by zero
+	constructor() {
+		super("/", 3, function (l, r) {
+			return l / r;
+		});
 	}
 }
 
@@ -26,25 +74,49 @@ app.controller("CalculatorController", function ($scope) {
 	$scope.formula = [];
 	$scope.result = "0";
 	$scope.grid = [
-		[ new Num(7), new Num(8), new Num(9), new Op("/") ],
-		[ new Num(4), new Num(5), new Num(6), new Op("*") ],
-		[ new Num(1), new Num(2), new Num(3), new Op("-") ],
-		[ new Num(0), new Input("."), new Input("="), new Op("+") ]
+		[ new Num(7), new Num(8), new Num(9), new DivOp() ],
+		[ new Num(4), new Num(5), new Num(6), new MulOp() ],
+		[ new Num(1), new Num(2), new Num(3), new SubOp() ],
+		[ new Num(0), new Special("."), new Special("="), new AddOp() ]
 	];
 
+	function isNum(x) {
+		return x !== undefined && x.value !== undefined;
+	}
+
+	function addToFormula(k) {
+		var last : Num = _.last($scope.formula);
+		if (isNum(k)) { // Num
+			if (isNum(last)) {
+				var kValue : number = k.value;
+				$scope.formula.pop(); // replace num
+				$scope.formula.push(new Num(last.value * 10 + kValue));
+			} else {
+				$scope.formula.push(k); // add num
+			}
+		} else { // BinaryOp
+			if (isNum(last)) {
+				$scope.formula.push(k); // add op
+			} else {
+				$scope.formula.pop(); // replace op
+				$scope.formula.push(k);
+			}
+		}
+	}
+
 	$scope.press = function (key) {
-		if (key.label === "=") {
+		if (key.display() === "=") {
 			$scope.result = evaluate($scope.formula)
-		} else if (key.label === ".") {
+		} else if (key.display() === ".") {
 			// TODO decimal point
 		} else {
-			$scope.formula.push(key);
+			addToFormula(key);
 		}
 	};
 	$scope.$on("keypress", function (keyCode) {
 		var pressedKey = String.fromCharCode(keyCode); // TODO arg is undefined
 		_.each($scope.grid, (row) => _.each(row, (key) => {
-			if (key.label === pressedKey) {
+			if (key.display() === pressedKey) {
 				$scope.press(key);
 			}
 		}));
